fba2c0c108c020ae2ac79869e976ec8d8cf2e2c0
Markus <markus@hellocash.at>
2020-08-30 18:02:07 +0200

    start rewrite

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import SchemaBuilder from './lib/schemaBuilder.js'
+
 // output folder where the generated .graphql schemes are saved
 const outputFolder = `output/Models/Generated/`
 
@@ -8,38 +10,39 @@ const outputFolder = `output/Models/Generated/`
 //  permissions:     see ./permissions folder
 
 const options = {
-    excludeTables: [
-        'jobs',
-        'permissions',
-        'order_positions_reporting_tags',
-        'migrations',
-        'password_resets',
-        'roles',
-        'model_has_roles',
-        'role_has_permissions',
-        'model_has_permissions',
-        'tracking_visits',
+  excludeTables: [
+    'jobs',
+    'permissions',
+    'order_positions_reporting_tags',
+    'migrations',
+    'password_resets',
+    'roles',
+    'model_has_roles',
+    'role_has_permissions',
+    'model_has_permissions',
+    'tracking_visits',
 
-        'article_package',
-        'discounts_packages',
-        'packages_categories',
-        'articles_attachments',
-        'invoice_cancellations',
-        'articles_reporting_tags',
-        'articles_search_parameters',
-        'packages_search_parameters',
-        'packages_attachments',
-        'customer_email_attachments',
-        'discounts_articles',
-    ],
-    singularExclude: [
-        'news'
-    ],
-    outputFolder: outputFolder,
-    overwrites: require(__dirname + '/lib/loaders/overwrites'),
-    permissions: require(__dirname + '/lib/loaders/permissions')
+    'article_package',
+    'discounts_packages',
+    'packages_categories',
+    'articles_attachments',
+    'invoice_cancellations',
+    'articles_reporting_tags',
+    'articles_search_parameters',
+    'packages_search_parameters',
+    'packages_attachments',
+    'customer_email_attachments',
+    'discounts_articles',
+  ],
+  singularExclude: ['news'],
+  outputFolder: outputFolder,
+  overwrites: require(__dirname + '/lib/loaders/overwrites'),
+  permissions: require(__dirname + '/lib/loaders/permissions'),
 }
 
-require('dotenv').config({ path: '.env' });
-const schemaBuilder  = require(__dirname + '/lib/schemaBuilder');
-schemaBuilder(options);
+require('dotenv').config({ path: '.env' })
+const schemaBuilder = require(__dirname + '/lib/schemaBuilder')
+schemaBuilder(options)
+
+const sb = SchemaBuilder
+console.log(sb)
